d109ac467818dab18be041378329e18cc0187df9
Yigit Yildirim <93987871+yigityildirimm@users.noreply.github.com>
2023-11-10 08:04:56 +0100

    Update movies_popularity.py

--- movies_popularity.py
+++ movies_popularity.py
@@ -6,23 +6,17 @@ movies_df = pd.read_csv('movies.csv')
 ratings_df = pd.read_csv('ratings.csv')
 tags_df = pd.read_csv('tags.csv')
 
-links_df.columns
 
-movies_df.columns
-
-ratings_df.columns
-
-tags_df.columns
 
 ratings_mc_df=ratings_df.groupby("movieId")["rating"].agg(["mean","count"]).reset_index()
-ratings_mc_df
 
-ratings_mc_df.sort_values(by=['count', 'mean'], ascending=False).head()
+
+#ratings_mc_df.sort_values(by=['count', 'mean'], ascending=False).head()
 
 ratings_mc_df["overall_rating"] = (ratings_mc_df["mean"]*2) + (ratings_mc_df["count"]*0.01)
-ratings_mc_df
 
-ratings_mc_df.sort_values(by=['overall_rating'], ascending=False).head()
+
+#ratings_mc_df.sort_values(by=['overall_rating'], ascending=False).head()
 
 ratings_mc_df_merged=ratings_mc_df.merge(
     movies_df,
